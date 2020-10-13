58f51c888824654620f20f38a6c9c4fe46445241
lakhansamani <lakhan.m.samani@gmail.com>
2020-10-14 00:56:29 +0530

    add class to get single ton esclient instance

---
+++ src/elasticsearch/client.ts
@@ -0,0 +1,20 @@
+import { Client } from '@elastic/elasticsearch';
+
+export class ESClient {
+  private static instance: Client;
+
+  public static getInstance(): Client {
+    if (ESClient.instance) {
+      console.log('returned from here...');
+      return ESClient.instance;
+    }
+    ESClient.instance = new Client({
+      node: process.env.ELASTICSEARCH_URL,
+      maxRetries: 5,
+      requestTimeout: 60000,
+      sniffOnStart: false,
+    });
+
+    return ESClient.instance;
+  }
+}

8157fd10bc7056e37db08136bc7a351850c3e1bd
Lakhan Samani <lakhan.m.samani@gmail.com>
2020-12-12 14:47:42 +0530

    fix return type for getSchema

--- src/schema.ts
+++ src/schema.ts
@@ -7,7 +7,7 @@ import { PingTypeDef } from './types/ping';
 import { PingResolver } from './resolvers/ping';
 import { JSONResolver } from './resolvers/json';
 
-export const getSchema = (esSchema: string) => {
+export const getSchema = (esSchema: string): any => {
   return makeExecutableSchema({
     typeDefs: [JSONTypeDef, ResponseTypeDef, PingTypeDef, esSchema],
     resolvers: { ...PingResolver, ...JSONResolver },
